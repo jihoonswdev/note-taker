c2188be78e36344c10e919e3f9b21fae86d44f26
Jason Park <jihoonswdev@gmail.com>
2021-09-27 11:59:35 -0400

    update note.js

--- routes/notes.js
+++ routes/notes.js
@@ -1,3 +1,28 @@
 const note = require('express').Router();
 const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
 const uuid = require('../helpers/uuid');
+
+note.get('/', (req, res) => {
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+});
+
+note.post('/', (req, res) => {
+    console.log(req.body);
+  
+    const { title, text } = req.body;
+  
+    if (req.body) {
+      const newNote = {
+        title,
+        text,
+        id: uuid()
+      };
+  
+      readAndAppend(newNote, './db/db.json');
+      res.json(`Note added successfully`);
+    } else {
+      res.error('Error in adding Note');
+    }
+});
+
+module.exports = note;
